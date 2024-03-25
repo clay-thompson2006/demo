cfd3676fdb9e321c0ea60fec49fd79bec730607d
clay-thompson2006 <143003832+clay-thompson2006@users.noreply.github.com>
2024-03-25 09:48:26 -0400

    3rd and 4th chapters

--- Test/home/views.py
+++ Test/home/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {} )
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_content = {'today': datetime.today()}
+
+class AuthurizedView(TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = ''
+
